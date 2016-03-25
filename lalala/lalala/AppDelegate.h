067d5dd45574174415005415acc7d88287ababbb
zd <lanou3g@lanou3gdeMac-mini90.local>
2016-03-25 14:23:03 +0800

    zd第一次修改

---
+++ lalala/lalala/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lalala
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 zd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
